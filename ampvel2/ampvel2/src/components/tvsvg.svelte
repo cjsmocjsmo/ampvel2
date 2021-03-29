2d3b4dfaba9737240154fa799101c8bcf5238f12
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-03-29 07:26:33 -0700

    added alien worlds and for all mankind

--- ampvel2/ampvel2/src/components/tvsvg.svelte
+++ ampvel2/ampvel2/src/components/tvsvg.svelte
@@ -4,28 +4,33 @@
 
 
 
-<svg viewBox="0 -5 300 120" >
+<svg viewBox="0 -5 300 175" >
 	<a href="alteredcarbon"><text x="0" y="7" font-size="1em" fill="grey">Altered Carbon</text></a>
-	<a href="lostinspace"><text x="135" y="7" font-size="1em" fill="grey">Lost In Space</text></a>
-	<a href="orville"><text x="251" y="7" font-size="1em" fill="grey">Orville</text></a>
+	<a href="orville"><text x="200" y="7" font-size="1em" fill="grey">Orville</text></a>
 
 	<a href="raisedbywolves"><text x="0" y="27" font-size="1em" fill="grey">Raised By Wolves</text></a>
-	<a href="sttv"><text x="135" y="27" font-size="1em" fill="grey">Star Trek</text></a>
-	<a href="voyager"><text x="241" y="27" font-size="1em" fill="grey">Voyager</text></a>
+	<a href="voyager"><text x="200" y="27" font-size="1em" fill="grey">Voyager</text></a>
 
 	<a href="nextgeneration"><text x="0" y="46" font-size="1em" fill="grey">Next Generation</text></a>
-	<a href="picard"><text x="135" y="46" font-size="1em" fill="grey">Picard</text></a>
-	<a href="mandalorian"><text x="209" y="46" font-size="1em" fill="grey">Mandalorian</text></a>
+	<a href="mandalorian"><text x="200" y="46" font-size="1em" fill="grey">Mandalorian</text></a>
 
 	<a href="discovery"><text x="0" y="64" font-size="1em" fill="grey">Discovery</text></a>
-	<a href="lowerdecks"><text x="100" y="64" font-size="1em" fill="grey">Lower Decks</text></a>
-	<a href="lastship"><text x="202" y="64" font-size="1em" fill="grey">The Last Ship</text></a>
+	<a href="lastship"><text x="200" y="64" font-size="1em" fill="grey">The Last Ship</text></a>
 
 	<a href="spacetime"><text x="0" y="82" font-size="1em" fill="grey">SpaceTime</text></a>
-	<a href="seancarrol"><text x="100" y="82" font-size="1em" fill="grey">Sean Carroll</text></a>
-	<a href="enterprise"><text x="223" y="82" font-size="1em" fill="grey">Enterprise</text></a>
+	<a href="enterprise"><text x="200" y="82" font-size="1em" fill="grey">Enterprise</text></a>
 
 	<a href="forallmankind"><text x="0" y="102" font-size="1em" fill="grey">For All Man Kind</text></a>
+	<a href="alienworlds"><text x="200" y="102" font-size="1em" fill="grey">Alien Worlds</text></a>
+
+	<a href="lostinspace"><text x="0" y="122" font-size="1em" fill="grey">Lost In Space</text></a>
+	<a href="sttv"><text x="200" y="122" font-size="1em" fill="grey">Star Trek</text></a>
+
+	<a href="picard"><text x="0" y="142" font-size="1em" fill="grey">Picard</text></a>
+	<a href="lowerdecks"><text x="200" y="142" font-size="1em" fill="grey">Lower Decks</text></a>
+
+
+	<a href="seancarrol"><text x="0" y="162" font-size="1em" fill="grey">Sean Carroll</text></a>
 
 	Sorry, your browser does not support inline SVG.  
 </svg>
---
+++ ampvel2/ampvel2/src/routes/alienworlds.svelte
@@ -0,0 +1,105 @@
+<script>
+    import Controls from '../components/Controls.svelte'
+    let AWorldsshowlist;
+    let AWorldsshowlistcount;
+    let visibleOrv = false
+
+    async function getAlienWorlds(x) {
+
+        let addr = `http://192.168.0.42:8888/intAlienWorlds?season=${x}`
+        fetch(addr, {mode: "cors", method: "GET"})
+        .then(r => r.json())
+        .then(data => {
+            AWorldsshowlist = data
+            AWorldsshowlistcount = data.length
+        }).catch(err => console.log(err));
+    }
+    let handleAlienWorlds1 = () => {
+        let promise = getAlienWorlds(`01`).catch(err => console.log(err));
+    }
+    // let handleFAMK2 = () => {
+    //     let promise = getAlienWorlds(`02`).catch(err => console.log(err));
+    // }
+    // let handleFAMK3 = () => {
+    //     let promise = getAlienWorlds(`03`).catch(err => console.log(err));
+    // }
+
+    // let fuckOrv = () => {
+    //     if (visibleOrv) {
+    //         visibleOrv = false
+    //     } else {
+    //         visibleOrv = true
+    //     }
+    // }
+
+    async function getPlayMedia(x) {
+        let y = "/media/pi/PiTB/media/TVShows" + x
+        let addr = `http://192.168.0.42:8888/playMediaReact?movie=${y}`
+        fetch(addr, {mode: "cors", method: "GET"})
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+        }).catch(err => console.log(err));
+    }
+
+    let handlePlayShowOr = (media) => {
+        // if (LP) {
+        //     let foo = media.split("TVShows", 2)
+        //     let newpath = `http://192.168.0.42:8063` + media
+        //     TVlocalplayURL.set(newpath)
+        //     visibleOrv = false
+        // } else {
+            let promise = getPlayMedia(media).catch(err => console.log(err));
+            visibleOrv = false
+        // }
+    }
+</script>
+<svelte:head>
+	<title>The Orville</title>
+</svelte:head>
+
+<h1>For All Mankind</h1>
+<Controls />
+<svg viewBox="0 0 273 34">
+    <rect on:click={handleAlienWorlds1} x="0" y="0" width="32%" height="30" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)" />
+    <rect on:click={handleFAMK2} x="97" y="0" width="32%" height="30" style="fill:red;stroke-width:1;stroke:rgb(0,0,0)" /> -->
+    <rect on:click={handleFAMK3} x="195" y="0" width="32%" height="30" style="fill:green;stroke-width:1;stroke:rgb(0,0,0)" />
+    <text on:click={handleAlienWorlds1} x="23" y="18" font-size=".7em" fill="white">Season 1</text>
+    <!-- <text on:click={handleFAMK2} x="120" y="18" font-size=".7em" fill="white">Season 2</text> -->
+    <!-- <text on:click={handleFAMK3} x="210" y="18" font-size=".7em" fill="white">Season 3</text> -->
+</svg> 
+
+<ul>
+    {#if AWorldsshowlistcount > 0}
+        {#each AWorldsshowlist as sshow }
+            <li>
+                <span>{sshow.episode}</span>
+                <a href="tvshows" on:click={handlePlayShowOr(sshow.tvfspath)}>{sshow.title}</a>
+            </li>
+        {/each}
+    {/if}
+</ul>
+<style>
+    span {
+        margin-right: 20px;
+        color: white;
+    }
+    ul {
+        list-style: none;
+    }
+
+    li {
+        margin: 10px;
+        text-decoration: none;
+        
+    }
+    a {
+        color:white;
+        font-size: 22px;
+    }
+
+    h1 {
+        color: white;
+    }
+
+</style>
