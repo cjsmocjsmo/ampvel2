5b9d49129d44911b10efbe56825203cc289fe269
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-04-03 10:24:24 -0700

    added falcon winter soldier

--- ampvel2/ampvel2/src/components/tvsvg.svelte
+++ ampvel2/ampvel2/src/components/tvsvg.svelte
@@ -4,34 +4,35 @@
 
 
 
-<svg viewBox="0 -5 300 175" >
-	<a href="alteredcarbon"><text x="0" y="7" font-size="1em" fill="grey">Altered Carbon</text></a>
-	<a href="orville"><text x="200" y="7" font-size="1em" fill="grey">Orville</text></a>
+<svg viewBox="0 -5 300 210" >
+	<a href="alteredcarbon"><text x="0" y="10" font-size="1em" fill="grey">Altered Carbon</text></a>
+	<a href="alienworlds"><text x="183" y="10" font-size="1em" fill="grey">Alien Worlds</text></a>
+	
+	<a href="discovery"><text x="0" y="30" font-size="1em" fill="grey">Discovery</text></a>
+	<a href="enterprise"><text x="183" y="30" font-size="1em" fill="grey">Enterprise</text></a>
+	
+	<a href="forallmankind"><text x="0" y="50" font-size="1em" fill="grey">For All Man Kind</text></a>
+	<a href="lastship"><text x="183" y="50" font-size="1em" fill="grey">Last Ship</text></a>
 
-	<a href="raisedbywolves"><text x="0" y="27" font-size="1em" fill="grey">Raised By Wolves</text></a>
-	<a href="voyager"><text x="200" y="27" font-size="1em" fill="grey">Voyager</text></a>
+	<a href="lostinspace"><text x="0" y="70" font-size="1em" fill="grey">Lost In Space</text></a>
+	<a href="lowerdecks"><text x="183" y="70" font-size="1em" fill="grey">Lower Decks</text></a>
 
-	<a href="nextgeneration"><text x="0" y="46" font-size="1em" fill="grey">Next Generation</text></a>
-	<a href="mandalorian"><text x="200" y="46" font-size="1em" fill="grey">Mandalorian</text></a>
+	<a href="mandalorian"><text x="0" y="90" font-size="1em" fill="grey">Mandalorian</text></a>
+	<a href="nextgeneration"><text x="183" y="90" font-size="1em" fill="grey">Next Generation</text></a>
 
-	<a href="discovery"><text x="0" y="64" font-size="1em" fill="grey">Discovery</text></a>
-	<a href="lastship"><text x="200" y="64" font-size="1em" fill="grey">The Last Ship</text></a>
+	<a href="orville"><text x="0" y="110" font-size="1em" fill="grey">Orville</text></a>
+	<a href="picard"><text x="183" y="110" font-size="1em" fill="grey">Picard</text></a>
 
-	<a href="spacetime"><text x="0" y="82" font-size="1em" fill="grey">SpaceTime</text></a>
-	<a href="enterprise"><text x="200" y="82" font-size="1em" fill="grey">Enterprise</text></a>
+	<a href="raisedbywolves"><text x="0" y="130" font-size="1em" fill="grey">Raised By Wolves</text></a>
+	<a href="seancarrol"><text x="183" y="130" font-size="1em" fill="grey">Sean Carroll</text></a>
 
-	<a href="forallmankind"><text x="0" y="102" font-size="1em" fill="grey">For All Man Kind</text></a>
-	<a href="alienworlds"><text x="200" y="102" font-size="1em" fill="grey">Alien Worlds</text></a>
+	<a href="spacetime"><text x="0" y="150" font-size="1em" fill="grey">SpaceTime</text></a>
+	<a href="sttv"><text x="183" y="150" font-size="1em" fill="grey">Star Trek</text></a>
 
-	<a href="lostinspace"><text x="0" y="122" font-size="1em" fill="grey">Lost In Space</text></a>
-	<a href="sttv"><text x="200" y="122" font-size="1em" fill="grey">Star Trek</text></a>
+	<a href="voyager"><text x="0" y="170" font-size="1em" fill="grey">Falcon Winter Soldier</text></a>
+	<a href="voyager"><text x="183" y="170" font-size="1em" fill="grey">Voyager</text></a>
 
-	<a href="picard"><text x="0" y="142" font-size="1em" fill="grey">Picard</text></a>
-	<a href="lowerdecks"><text x="200" y="142" font-size="1em" fill="grey">Lower Decks</text></a>
-
-
-	<a href="seancarrol"><text x="0" y="162" font-size="1em" fill="grey">Sean Carroll</text></a>
-
+	<a href="wandavision"><text x="0" y="190" font-size="1em" fill="grey">WandaVision</text></a>
 	Sorry, your browser does not support inline SVG.  
 </svg>
 
---
+++ ampvel2/ampvel2/src/routes/wandavision.svelte
@@ -0,0 +1,127 @@
+<script>
+    import Controls from '../components/Controls.svelte'
+    let showlist;
+    let showlistcount;
+    let visibleDis = false
+
+    // onMount(
+        async function getWandaVision(x) {
+            let addr = `http://192.168.0.42:8888/intWandaVision?season=${x}`
+            fetch(addr, {mode: "cors", method: "GET"})
+            .then(r => r.json())
+            .then(data => {
+                console.log(data)
+                console.log(data[0].episode)
+                showlist = data
+                showlistcount = data.length
+            }).catch(err => console.log(err));
+        }
+    //)
+
+    let handleWandaVision1 = () => {
+        let promise = getWandaVision(`01`).catch(err => console.log(err));
+    }
+
+    let handleWandaVision2 = () => {
+        let promise = getWandaVision(`02`).catch(err => console.log(err));
+    }
+
+    let fuckDis = () => {
+        if (visibleDis) {
+            visibleDis = false
+        } else {
+            visibleDis = true
+        }
+    }
+
+    async function getPlayMedia(x) {
+        let y = "/media/pi/PiTB/media/TVShows" + x
+        console.log("this is y")
+        console.log(y)
+        let addr = `http://192.168.0.42:8888/playMediaReact?movie=${y}`
+        console.log(addr)
+        fetch(addr, {mode: "cors", method: "GET"})
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+        }).catch(err => console.log(err));
+    }
+
+     let handlePlayShow = (media) => {
+        // if (LP) {
+        //     console.log(media)
+
+        //     let foo = media.split("TVShows", 2)
+        //     let newpath = `http://192.168.0.42:8063` + "/" + media
+        //     console.log(newpath)
+        //     TVlocalplayURL.set(newpath)
+        //     visibleDis = false
+        // } else {
+            let promise = getPlayMedia(media).catch(err => console.log(err));
+            visibleDis = false
+        // }
+        
+    }
+</script>
+<svelte:head>
+	<title>WandaVision</title>
+</svelte:head>
+
+
+<h1>WandaVision</h1>
+<Controls />
+<svg viewBox="0 0 273 35">
+    <rect on:click={handleWandaVision1} x="0" y="0" width="49%" height="30" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)" />
+    <rect on:click={handleWandaVision2} x="135" y="0" width="49%" height="30" style="fill:red;stroke-width:1;stroke:rgb(0,0,0)" />
+    <text on:click={handleWandaVision1} x="48" y="18" font-size=".7em" fill="white">Season 1</text>
+    <text on:click={handleWandaVision2} x="177" y="18" font-size=".7em" fill="white">Season 2</text>
+</svg> 
+<!-- <nav>
+    <button>Season 1</button>
+    <button>Season 2</button>
+</nav> -->
+<!-- <nav>
+    <button class="boo">Season 1</button>
+    <button class="boo">Season 2</button>
+    <button class="boo">Season 3</button>
+</nav> -->
+<!-- <nav>
+    <button on:click={handleWandaVision1}>s1</button>
+    <button on:click={handleWandaVision2}>s2</button>
+</nav> -->
+<ul>
+    {#if showlistcount > 0}
+        {#each showlist as sshow }
+            <li>
+                <span>{sshow.episode}</span>
+                <a href="tvshows" on:click={handlePlayShow(sshow.tvfspath)}>{sshow.title}</a>
+            </li>
+        {/each}
+    {/if}
+</ul>
+
+<style>
+    span {
+        margin-right: 20px;
+        color: white;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    li {
+        margin: 10px;
+        text-decoration: underline;
+    }
+
+    a {
+        color:white;
+        font-size: 22px;
+    }
+
+
+    h1 {
+        color: white;
+    }
+</style>
